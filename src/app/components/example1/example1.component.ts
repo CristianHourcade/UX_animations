cefa7f1929cd94890812febd153c564bd721d609
Cristian Hourcade <chourcade@baufest.com>
2019-09-04 10:21:38 -0300

    ;)

--- src/app/components/example1/example1.component.ts
+++ src/app/components/example1/example1.component.ts
@@ -8,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class Example1Component implements OnInit {
 
   siguiente = false;
-  
+  stepTercerActive = "";
+  isMenu = false;
+  tercerPaso = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+
+  setearTercerTiempo(){
+    this.siguiente = true;
+    setTimeout(() => {
+      this.stepTercerActive = "tercer_paso";
+      this.tercerPaso = true;
+    }, 1800);
+  }
 }
